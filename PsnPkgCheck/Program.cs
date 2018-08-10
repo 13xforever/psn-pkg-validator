da7d75f0159fc1dcb1cf16b85ad32401787f1e0f
13xforever <ilya.veselov@gmail.com>
2018-08-10 23:03:38 +0500

    Add project files.

---
+++ PsnPkgCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PsnPkgCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
